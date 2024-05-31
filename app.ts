26ff6133a131f4cd419782494bff7045ef572677
lubna-khan-github <lk4231455@outlook.com>
2024-05-31 13:12:20 +0500

    push d15k_word_counter on github

---
+++ app.ts
@@ -0,0 +1,26 @@
+#!  /usr/bin/env node
+
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+const answer:{sentence:string} = await inquirer.prompt([{
+    name: "sentence",
+    message:chalk.green.italic ("enter your sentence to count words: "),
+    type: "input"
+
+}]);
+// ***************** USER INPUT *****************************//
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.blue.bold(`your sentence word count is ${words.length}`));
+
+
+
+
+
+
+
+
+
+
